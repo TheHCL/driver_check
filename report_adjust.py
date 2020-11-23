891be82eddd8321ef39bcf2325a3ae98b8cd79c3
TheHCL <daniel8411222@gmail.com>
2020-11-22 23:53:32 -0600

    Modified code and upload exe

--- report_adjust.py
+++ report_adjust.py
@@ -3,6 +3,7 @@ import openpyxl
 import tkinter
 from tkinter import messagebox
 
+
 def run():
     if os.path.exists("settings.txt"):
         setting = open("settings.txt")
@@ -34,6 +35,26 @@ def excel_adjust():
     row_count = current_sheet.max_row
     for i in range(0,int(row_count/2)):
         current_sheet.cell(row=2*i+1,column=1).value = i+1
+    for i in range(0,row_count,2):  #merge row
+        current_sheet.merge_cells(start_row=i+1,start_column=1,end_row=i+2,end_column=1)
+        
+    
+    for i in range(1,sheet.max_column+1):  # column width adjust
+        max_val =0
+        for cell in sheet[openpyxl.utils.get_column_letter(i)]:
+            val_to_check = len(str(cell.value))
+            if val_to_check>max_val:
+                max_val=val_to_check
+        sheet.column_dimensions[openpyxl.utils.get_column_letter(i)].width = max_val+1
+    for i in range(0,row_count):
+        for k in range(0,current_sheet.max_column):
+            if (i%4==0 )or (i%4 ==1):
+                current_sheet.cell(row=i+1,column=k+1).fill = openpyxl.styles.PatternFill(start_color="FFECC9",end_color="FFECC9",fill_type="solid")
+            else :
+                current_sheet.cell(row=i+1,column=k+1).fill = openpyxl.styles.PatternFill(start_color="FFDEDE",end_color="FFDEDE",fill_type="solid")
+            # make thick border
+            current_sheet.cell(row=i+1,column=k+1).border = openpyxl.styles.Border(left=openpyxl.styles.Side(style="thick"),right=openpyxl.styles.Side(style="thick"),top=openpyxl.styles.Side(style="thick"),bottom=openpyxl.styles.Side(style="thick"))
+
     wb.save(report_name)
 
 
